c0e7e299d6016576a8e082fa8a9c3740c0e21672
Abolfazl Z <87231228+DevAbolfazl@users.noreply.github.com>
2024-07-06 18:44:44 +0330

    Create hwlds256.go

---
+++ hwlds256.go
@@ -0,0 +1,42 @@
+/*
+	HWLDS (Hash with length-dependent security)
+	256 Bits
+*/
+
+package hash_algos
+
+import (
+	"encoding/hex"
+	"errors"
+)
+
+func hwlds256(inputString string) string {
+	if inputString == "" {
+		_ = errors.New("HWLDS Hash Error: Empty input")
+	}
+	var result []byte = []byte(inputString)
+	var helpers = []byte{0b00000000}
+	for i := 0; i < 64-len(result); i++ {
+		result = append(result, helpers[0])
+	}
+	var x int = 0
+	for y := range result {
+		if x == len(helpers) {
+			x = 0
+		}
+		result[y] ^= helpers[x]&result[y] | (result[y] << 2)
+		for z := len(result) - 1; z >= 0; z-- {
+			helpers = append(helpers, result[y]^result[z]<<helpers[x])
+			for h := range helpers {
+				helpers[h] = helpers[h] ^ helpers[x+1]
+			}
+			for h := range helpers {
+				result[x] = result[y] ^ helpers[h]
+			}
+			result[z] = result[z] ^ (helpers[x] | result[y])
+			result[y] ^= result[z]
+		}
+		x++
+	}
+	return hex.EncodeToString(result[len(result)-33 : len(result)-1])
+}
